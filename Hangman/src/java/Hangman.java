2f4225d7ceedb1b2ead175a4524e0578202a0cf5
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-07-08 18:10:44 -0400

    wip: functionality and game complete
    still need to create test

--- Hangman/src/java/Hangman.java
+++ Hangman/src/java/Hangman.java
@@ -1,9 +1,137 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Hangman {
-    public static void main() {
-        Scanner scanner= new Scanner(System.in);
-        Random random= new Random();
+    String[] words= {"genspark", "software", "engineer", "jazz", "cat"};
+    static Scanner ui= new Scanner(System.in);
+
+    int attempt= 0;
+    boolean gmaeDone= false;
+    static boolean play= true;
+
+    List<Character> playerGuesses= new ArrayList<>();
+
+    public static void main(String[] args) {
+        Hangman hm= new Hangman();
+        while(play) {
+            hm.play();
+        }
     }
+
+    public void play() {
+
+        String wordToFind = getRandomWord();
+        try{
+            while(!gmaeDone) {
+                System.out.println("H A N G M A N");
+                printGallows(attempt);
+                if(attempt>= 5) {
+                    System.out.println("You lose");
+                    System.out.printf("The word was: %s", wordToFind);
+                    break;
+                }
+                printGuessingState(wordToFind, playerGuesses);
+                if(!getPlayerInput(ui, wordToFind, playerGuesses)) {
+                    attempt++;
+                }
+                if(printGuessingState(wordToFind, playerGuesses)) {
+                    System.out.printf("\nYes! The secret word is \"%s\"! You have won!", wordToFind);
+                    System.out.println("\nDo you want to play again? (yes or no)");
+                    play= ui.nextLine().trim().equalsIgnoreCase("yes");
+                    if(play) {
+                        gmaeDone= false;
+                        playerGuesses.clear();
+                        wordToFind= getRandomWord();
+                        attempt= 0;
+                    } else {
+                        break;
+                    }
+                }
+
+            }
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    String getRandomWord() {
+        Random rand= new Random(); // get random word from 'words arr'
+        int randInt= rand.nextInt(words.length);
+        return words[randInt];
+    }
+
+    private static boolean printGuessingState(String wordToFind, List<Character> playerGuesses) {
+        int correctCount= 0;
+
+        for(int i= 0; i< wordToFind.length(); i++) { // if user input matches a letter, insert into blank spaces
+            if(playerGuesses.contains(wordToFind.charAt(i))) {
+                System.out.print(wordToFind.charAt(i));
+                correctCount++;
+            } else {
+                System.out.print("_ "); // if not leave blank spaces
+            }
+        }
+        System.out.println();
+
+        return (wordToFind.length()== correctCount); // return true if user is able to enter all correct letters
+    }
+
+    private static boolean getPlayerInput(Scanner ui, String wordToFind, List<Character> playerGuesses) {
+        System.out.println("Guess a letter: ");
+        String guessInput= ui.nextLine();
+        playerGuesses.add(guessInput.charAt(0)); // takes the first letter of a user input
+
+        return (wordToFind.contains(guessInput));
+    }
+
+    public static void printGallows(int attempt) {
+        System.out.println("      _______");
+        switch (attempt) {
+            case 1:
+                System.out.print(
+                        "     |/      |\n" +
+                                "     |      \n" +
+                                "     |      \n" +
+                                "     |       \n" +
+                                "     |      \n");
+                break;
+            case 2:
+                System.out.print("     |/      |\n"
+                        + "     |       o \n"
+                        + "     |      \n"
+                        + "     |       \n"
+                        + "     |      \n");
+                break;
+            case 3:
+                System.out.print("     |/      |\n"
+                        + "     |       o \n"
+                        + "     |      \\|/\n"
+                        + "     |       \n"
+                        + "     |      \n");
+                break;
+            case 4:
+                System.out.print("     |/      |\n"
+                        + "     |       o \n"
+                        + "     |      \\|/\n"
+                        + "     |       |\n"
+                        + "     |      \n");
+                break;
+            case 5:
+                System.out.print("     |/      |\n"
+                        + "     |       o \n"
+                        + "     |      \\|/\n"
+                        + "     |       |\n"
+                        + "     |      / \\\n");
+                break;
+            default:
+                System.out.print("     |/      \n"
+                        + "     |      \n"
+                        + "     |      \n"
+                        + "     |       \n"
+                        + "     |      \n");
+                break;
+        }
+        System.out.println("     |");
+        System.out.println(" ____|___");
+    }
+
 }
