cb962c2a4b8b9ce8d23a1fe74ebdf076ddc629ec
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-08-09 15:05:54 -0400

    wip: RESTful API starting with employee db

---
+++ Spring/RESTful/Employee/src/main/java/com/genspark/Employee/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+	// RESTful API ~ convention to build HTTP Web Services
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
