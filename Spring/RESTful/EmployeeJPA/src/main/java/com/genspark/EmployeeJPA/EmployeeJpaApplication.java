32537eddfe1e5e35ad33752817c90e934c98ccc0
elvisEspinoza <elvis_espinoza98@yahoo.com>
2022-08-09 23:57:45 -0400

    wip: second RESTful API using JPA

---
+++ Spring/RESTful/EmployeeJPA/src/main/java/com/genspark/EmployeeJPA/EmployeeJpaApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.EmployeeJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeJpaApplication.class, args);
+	}
+
+}
---
+++ Spring/RESTful/EmployeeJPA/src/test/java/com/genspark/EmployeeJPA/EmployeeJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.EmployeeJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
